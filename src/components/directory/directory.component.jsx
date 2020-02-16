b0503402ed2c7abc05ec5748bf157c24646dc37b
Vigan Zeqiri <viganzeqirii@gmail.com>
2020-02-16 01:32:46 +0100

    Routing added

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,13 +49,8 @@ class Directory extends React.Component {
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ title, imageUrl, id, size }) => (
-					<MenuItem
-						key={id}
-						title={title}
-						imageUrl={imageUrl}
-						size={size}
-					/>
+				{this.state.sections.map(({ id, ...restProps }) => (
+					<MenuItem key={id} {...restProps} />
 				))}
 			</div>
 		);
