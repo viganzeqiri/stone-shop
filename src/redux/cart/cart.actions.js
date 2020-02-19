f0978544b959873707f966fdb870b8cd686c508b
Vigan Zeqiri <viganzeqirii@gmail.com>
2020-02-19 10:13:26 +0100

    Added add and remove arrows in chceckout item

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = item => ({
 	type: CartActionTypes.ADD_ITEM,
 	payload: item
 });
+
+export const removeItem = item => ({
+	type: CartActionTypes.REMOVE_ITEM,
+	payload: item
+});
+
+export const removeItemFromCart = item => ({
+	type: CartActionTypes.REMOVE_ITEM_FROM_CART,
+	payload: item
+});
