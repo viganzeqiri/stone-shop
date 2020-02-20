934c55047d74b97a20ab22df4c51b28a87137bf1
Vigan Zeqiri <viganzeqirii@gmail.com>
2020-02-20 16:00:52 +0100

    Added stripe integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -38,6 +39,12 @@ const ChackoutPage = ({ cartItems, total }) => {
 			<div className="total">
 				<span>Total: ${total}</span>
 			</div>
+			<div className="test-warning">
+				*Please use the following test credit card for payments
+				<br />
+				4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+			</div>
+			<StripeCheckoutButton price={total} />
 		</div>
 	);
 };
@@ -47,6 +54,4 @@ const mapStateToProps = createStructuredSelector({
 	total: selectCartItemsTotal
 });
 
-const mapDispatchToProps = dispatch => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ChackoutPage);
+export default connect(mapStateToProps)(ChackoutPage);
