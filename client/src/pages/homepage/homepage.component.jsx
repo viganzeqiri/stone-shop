2adda0e46488b8640de86270b59e8d8292f00911
Vigan Zeqiri <viganzeqirii@gmail.com>
2020-03-03 08:38:15 +0100

    updated our code to be pwa compliant, added service worker, https, updated manifest

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Profiler } from "react";
 
 import { HomepageContainer } from "./homepage.styles";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
 	<HomepageContainer>
-		<Directory />
+		<Profiler
+			id="Directory"
+			onRender={(id, phase, actualDuration) =>
+				console.log({
+					id,
+					phase,
+					actualDuration
+				})
+			}
+		>
+			<Directory />
+		</Profiler>
 	</HomepageContainer>
 );
 
